fc926f6c41a365b7ebf2b4a5bd9ef611e1f8aca4
Abdulsobur <91859818+Abdulsobur-tech@users.noreply.github.com>
2022-08-01 21:25:40 +0100

    Initial commit

---
+++ src/brackets/index.js
@@ -0,0 +1,24 @@
+/**
+ * Implement the solution to brackets
+ *
+ * @param {string} str The string of brackets.
+ * @returns {"valid" | "invalid"} Whether or not the string is valid.
+//  */
+ function isValid (str) {
+    const  obj = {"(" : ")", "{" : "}", "[": "}"}
+    const newArray= []
+    for(let char of str){
+        if(obj[char]){
+            newArray.push(obj[char])
+        }else if(newArray.length > 0 ){
+            newArray.pop()
+        }else{
+            return "invalid"
+        }
+    }
+    return "valid"
+   
+ }
+
+isValid("(([)])]");
+ module.exports = isValid;
